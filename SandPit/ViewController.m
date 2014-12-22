e4a4689b01bddc208e623698dc48e3944072de71
paulthomasww <paul@Pauls-MacBook-Pro.local>
2014-12-22 11:56:35 +1100

    Removed unused classes and created basic user interaction

--- SandPit/ViewController.m
+++ SandPit/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UIButton *button;
 @end
 
 @implementation ViewController
@@ -17,11 +17,37 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    CGRect frame = CGRectMake(20.0f, 20.0f, 200.0f, 20.0f);
+    self.button = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.button.backgroundColor = [UIColor clearColor];
+    [self.button setFrame:frame];
+    [self.button setTitle:@"Click me!" forState:UIControlStateNormal];
+    [self.button setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    [self.button addTarget:self action:@selector(labelTap:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:self.button];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    
+    [super viewDidAppear:animated];
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)labelTap:(id) sender {
+    
+    NSString *title = @"Default title";
+    if ([sender isKindOfClass:[UIButton class]]){
+        UIButton *button = (UIButton *)sender;
+        title = button.titleLabel.text;
+    }
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:title message:@"Do you really want to do that?" delegate:nil cancelButtonTitle:@"Cancel" otherButtonTitles:@"Ok", nil];
+    
+    [alert show];
 }
 
 @end
